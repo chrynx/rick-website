1cf12a86a487b2c764f74870df7d638d08c2160a
chrynx <ralphmadriaga@gmail.com>
2017-12-09 02:49:40 +0000

    fixed the basic website

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './scss/style.scss';
 class App extends React.Component {
 
   render() {
     return (
-      <h1>WDI Project 4: MERN Stack App</h1>
+      <div className="main">
+      <nav id="top">
+        <div className="homeButton">
+        <a href="#top"><p>Home</p></a>
+        </div>
+        <div className="navLinks">
+          <ul>
+            <a href="#cars"><li><p>Cars</p></li></a>
+            <a href="#about"><li><p>About</p></li></a>
+            <a href="#contact"><li><p>Contact</p></li></a>
+          </ul>
+        </div>
+      </nav>
+      <div className="jumbotron">
+        <h1>Rick's Classic Cars</h1>
+      </div>
+    </div>
     );
   }
 }
